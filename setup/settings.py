95b3e991f70bbffd3bfe9d946b1aee9342074436
celioalves <craftsman7@gmail.com>
2023-03-25 07:44:39 -0300

    Adicionada o caminho para a aplicação usuarios nos INSTALLED APPS

--- setup/settings.py
+++ setup/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
-
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
