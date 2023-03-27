2defcfacdf58379746ebfcc750da01e66a4abf4a
celioalves <craftsman7@gmail.com>
2023-03-27 08:52:28 -0300

    adicionados os atributos de form-control e placeholder, tanto no nome de login, quanto na senha

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,12 +4,23 @@ class LoginForms(forms.Form):
     name_login=forms.CharField(
         label='Nome de Login',
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                'placeholder': 'Ex.: João da Silva'
+            }
+        )
     )
 
     senha=forms.CharField(
         label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                'placeholder': "Digite sua senha"
+            }
+        )
     )
